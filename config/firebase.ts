4a1715da8c6d0f783756dd8e13ddea98a40b1fa0
Keziah <kezziahngugi2000@gmail.com>
2023-05-25 10:43:35 +0300

    FirstDeploy

---
+++ config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7XwMFZMXAgg7_HhOU69CVyt4yjF-nRjQ",
+  authDomain: "next-auth-sample-b0793.firebaseapp.com",
+  projectId: "next-auth-sample-b0793",
+  storageBucket: "next-auth-sample-b0793.appspot.com",
+  messagingSenderId: "208779581688",
+  appId: "1:208779581688:web:fc4cf94d0504af11d6430d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
